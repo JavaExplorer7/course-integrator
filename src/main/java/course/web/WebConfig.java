8f72dc54b281b28c45f26b4740606bb5f6ce8122
jacob <jacob@jacob-machine>
2019-12-15 14:45:03 +0800

    welcome page added

--- src/main/java/course/web/WebConfig.java
+++ src/main/java/course/web/WebConfig.java
@@ -9,6 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("welcome");
 		registry.addViewController("/login").setViewName("login");
 		registry.addViewController("/about").setViewName("about");
 		registry.addViewController("/contact").setViewName("contact");
--- src/main/resources/static/js/share.js
+++ src/main/resources/static/js/share.js
@@ -5,10 +5,21 @@
 
 $(function() {
 	
+	// do the fast load among about, contact and welcome tabs
+	$('nav a').on('click', function(e) {
+		var href = this.href;
+		var page = href.substring(href.lastIndexOf("/") + 1);
+
+		if (page !== 'home' && page !== 'warehouse') {
+			e.preventDefault();
+			var $this = $(this);
+			
+			$('nav a.current').removeClass('current');
+			$this.addClass('current');
+			
+			$('#container').remove();
+			$('#content').load(href + ' #container').hide().fadeIn('slow');
+		}
+	});
+
 });
-
-
-/* Load new content into page */
-function loadContent(url) {
-  $('#content').load(url + ' #container').hide().fadeIn('slow');
-}
